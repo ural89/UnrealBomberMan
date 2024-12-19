06f68276f79df1655f8147f555f974c48dc53bd9
ural89 <uralkemal@gmail.com>
2024-12-19 18:22:41 +0300

    starter content

---
+++ Source/BomberMan/BomberCharacter.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BomberCharacter.generated.h"
+
+UCLASS()
+class BOMBERMAN_API ABomberCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABomberCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
